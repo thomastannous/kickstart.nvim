f560427d47bd74a63d257380ea04244eda3f7ad4
Thomas <ttohmas@gmail.com>
2024-03-23 08:58:44 +0900

    add some fuzzy search config

--- init.lua
+++ init.lua
@@ -414,6 +414,14 @@ require('telescope').setup {
     lsp_references = {
       show_line = false
     }
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+    }
   }
 }
 
